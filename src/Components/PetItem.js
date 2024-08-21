60b0cbce965d191d4490edbab58e91f47056c136
Dena <denaalt2004@gmail.com>
2024-08-21 15:53:02 +0300

    task part 1 done p2 still

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,12 +1,20 @@
+import React, { useState } from "react";
+import petsData from "../petsData";
+
 function PetItem({ pet }) {
+  const [imge, setImge] = useState(pet.image);
+  const changee = () => {
+    setImge(pet.image2);
+  };
+
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
       <div className="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={imge} />
         <div className="content">
           <h3>{pet.name}</h3>
-          <button type="button" className="btn btn-info">
-            Pet
+          <button onClick={changee} type="button" className="btn btn-info">
+            pet
           </button>
           <button type="button" class="btn btn-info  m-2">
             Adopt
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,45 +1,50 @@
 import pets from "../petsData";
 import PetItem from "./PetItem";
+//import React, { useState } from "react";
 
 function PetsList() {
   const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
-
-  return (
-    <section id="doctors" className="doctor-section pt-140">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-xxl-5 col-xl-6 col-lg-7">
-            <div className="section-title text-center mb-30">
-              <h1 className="mb-25 wow fadeInUp" data-wow-delay=".2s">
-                Fur-ends
-              </h1>
-              <div className="input-group rounded">
-                <input
-                  type="search"
-                  className="form-control rounded"
-                  placeholder="Search"
-                  aria-label="Search"
-                  aria-describedby="search-addon"
-                />
-              </div>
-              <br />
-              Type:
-              <select className="form-select">
-                <option value="" selected>
-                  All
-                </option>
-                <option value="Cat">Cat</option>
-                <option value="Dog">Dog</option>
-                <option value="Rabbit">Rabbit</option>
-              </select>
+  //const petlist =  petList ({ pets }) => {
+  // const [query, setQuery] = useState("");
+  //const filteredPets = pets.filter((pet) =>
+  // pet.name.toLowerCase().includes(query.to - toLowerCase())
+  // );
+}
+   return (
+  <section id="doctors" className="doctor-section pt-140">
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-xxl-5 col-xl-6 col-lg-7">
+          <div className="section-title text-center mb-30">
+            <h1 className="mb-25 wow fadeInUp" data-wow-delay=".2s">
+              Fur-ends
+            </h1>
+            <div className="input-group rounded">
+              <input
+                type="search"
+                className="form-control rounded"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="search-addon"
+              />
             </div>
+            <br />
+            Type:
+            <select className="form-select">
+              <option value="" selected>
+                All
+              </option>
+              <option value="Cat">Cat</option>
+              <option value="Dog">Dog</option>
+              <option value="Rabbit">Rabbit</option>
+            </select>
           </div>
         </div>
-
-        <div className="row justify-content-center">{petList}</div>
       </div>
-    </section>
-  );
-}
+
+      <div className="row justify-content-center">{petList}</div>
+    </div>
+  </section>
+);
 
 export default PetsList;
